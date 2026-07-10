fda3a9a7d0cd252d8b0a1c9afcf717b8668b7956
madhupriya-1787 <priyachowdary1787@gmail.com>
2026-07-10 23:15:27 +0530

    Update S3 bucket name in backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "daily-transaction-processing-21492298"
+    bucket       = "daily-transaction-processing-441661110146"
     key          = "terraform/terraform.tfstate"
     region       = "ap-southeast-2"
     use_lockfile = true
