c237e305e28778cef8f803e4a5bf81dd72b42631
madhupriya-1787 <priyachowdary1787@gmail.com>
2026-07-10 23:14:31 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "daily-transaction-processing-<account-id>"
+    bucket       = "daily-transaction-processing-21492298"
     key          = "terraform/terraform.tfstate"
     region       = "ap-southeast-2"
     use_lockfile = true
