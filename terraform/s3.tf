ded81b22f91099ea9bf5e815767b5590cdd36eb7
madhupriya-1787 <priyachowdary1787@gmail.com>
2026-07-11 12:01:14 +0530

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,14 +1,13 @@
-environment = [
-  {
-    name  = "BUCKET_NAME"
-    value = data.aws_s3_bucket.transaction_bucket.bucket
-  },
-  {
-    name  = "INPUT_KEY"
-    value = "input/transactions.csv"
-  },
-  {
-    name  = "OUTPUT_KEY"
-    value = "output/daily_transaction_summary.csv"
-  }
-]
+data "aws_s3_bucket" "transaction_bucket" {
+  bucket = "daily-transaction-processing-441661110146"
+}
+
+resource "aws_s3_object" "input_folder" {
+  bucket = data.aws_s3_bucket.transaction_bucket.id
+  key    = "input/"
+}
+
+resource "aws_s3_object" "output_folder" {
+  bucket = data.aws_s3_bucket.transaction_bucket.id
+  key    = "output/"
+}
