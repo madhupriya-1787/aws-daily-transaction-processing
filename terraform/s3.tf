c408d38fca3ddf6177b337f09574e30d79eb05c4
madhupriya-1787 <priyachowdary1787@gmail.com>
2026-07-11 11:52:55 +0530

    Refactor S3 configuration with environment variables
    Removed S3 bucket resource definitions and added environment variables for bucket and key names.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,18 +1,14 @@
-data "aws_s3_bucket" "transaction_bucket" {
-  bucket = "daily-transaction-processing-441661110146"
-}
-
-resource "aws_s3_object" "input_folder" {
-  bucket = data.aws_s3_bucket.transaction_bucket.id
-  key    = "input/"
-}
-
-resource "aws_s3_object" "intermediate_folder" {
-  bucket = data.aws_s3_bucket.transaction_bucket.id
-  key    = "intermediate/"
-}
-
-resource "aws_s3_object" "output_folder" {
-  bucket = data.aws_s3_bucket.transaction_bucket.id
-  key    = "output/"
-}
+environment = [
+  {
+    name  = "BUCKET_NAME"
+    value = data.aws_s3_bucket.transaction_bucket.bucket
+  },
+  {
+    name  = "INPUT_KEY"
+    value = "input/transactions.csv"
+  },
+  {
+    name  = "OUTPUT_KEY"
+    value = "output/daily_transaction_summary.csv"
+  }
+]
