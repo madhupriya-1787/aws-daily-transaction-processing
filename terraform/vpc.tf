62d15ce4b7b733b24c95eec52de789e61c57b07c
madhupriya-1787 <priyachowdary1787@gmail.com>
2026-07-14 10:32:59 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -21,19 +21,4 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-resource "aws_security_group" "ec2_sg" {
-  name        = "${var.project_name}-ec2-sg"
-  description = "Security group for EC2 check instance"
-  vpc_id      = aws_default_vpc.default.id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-ec2-sg"
-  }
-}
